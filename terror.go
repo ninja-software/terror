f51fdd39d2b244fa95cc3bf988418c161b77ca0d
Mac Ma <mac01@runbox.com>
2022-03-12 02:47:12 +0800

    version bump

--- terror.go
+++ terror.go
@@ -14,7 +14,7 @@ import (
 )
 
 // baseVersion actual version of terror module
-const baseVersion string = "v2.0.11"
+const baseVersion string = "v2.0.12"
 
 // ErrKind Kind of error
 type ErrKind string
